8f2ce7db373b58990dfd258fc2a82a636ebb23aa
AMEXXX <b123013@iiit-bh.ac.in>
2025-09-03 19:48:16 +0530

    data modeling

--- L3 Data Modelling with Mongoose/models/user.models.js
+++ L3 Data Modelling with Mongoose/models/user.models.js
@@ -1,23 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
 const userSchema = new mongoose.Schema(
-    {
-        username : {
-            type : String,
-            required : true,
-            unique : true
-        },
-        password : {
-            type : String,
-            required : [ true , "Password is required"],
-            minlength : [6, "Password must be at least 6 characters long"]
-        },
-        email : {
-            type : String,
-            required : [true, "Email is required"],
-            lowercase : true,
-            unique :true,
-            
-        }
-    },{timestamps:true}
-)
-export const User = mongoose.model("User",userSchema);
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
